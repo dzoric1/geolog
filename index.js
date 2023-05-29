083c859d198d70aa7db619787652072a8d1ef501
dzoric1 <Gerasimov2099@mail.ru>
2023-05-29 19:30:12 +0800

    realized popup

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const burgerBar = document.querySelector('.burger__bar')
   const headerNav = document.querySelector('.header__nav')
   const burgerBtn = document.querySelector('.burger')
+  const profileBtn = document.querySelector('.header__profile-link')
+  const popup = document.querySelector('.popup')
+  const popupCloseBtn = document.querySelector('.popup__close')
 
   burgerBtn.addEventListener('mousedown', () => {
     headerNav.classList.toggle('header__nav--open')
     burgerBar.classList.toggle('burger__bar--close')
   })
+
+  profileBtn.addEventListener('mousedown', () => {
+    popup.classList.add('popup--opened')
+  })
+
+  popupCloseBtn.addEventListener('mousedown', () => {
+    popup.classList.remove('popup--opened')
+  })
+
 })
